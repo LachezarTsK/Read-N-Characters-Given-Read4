65a4f3f7612e7023aaba8724d891e1811b54c932
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-11-12 19:25:39 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,44 @@
+
+using System;
+
+/**
+ * ----- Comment from Leetcode -----
+ * 
+ * The Read4 API is defined in the parent class Reader4.
+ *     int Read4(char[] buf4);
+ */
+
+public class Solution : Reader4
+{
+    private static readonly int MAX_READ_CHARS_PER_CALL_OF_READ4 = 4;
+    private static readonly char[] buffer = new char[MAX_READ_CHARS_PER_CALL_OF_READ4];
+
+    private int numberOfActualCharsRead;
+    private int numberOfRemainingOfCharsToRead;
+
+    public int Read(char[] destinationBuffer, int numberOfCharsToRead)
+    {
+        numberOfRemainingOfCharsToRead = numberOfCharsToRead;
+
+        int numberOfReadChars = Read4(buffer);
+        UpdateDestinationBuffer(destinationBuffer, numberOfReadChars);
+
+        while (numberOfReadChars == 4 && numberOfRemainingOfCharsToRead > 0)
+        {
+            numberOfReadChars = Read4(buffer);
+            UpdateDestinationBuffer(destinationBuffer, numberOfReadChars);
+        }
+
+        return numberOfActualCharsRead;
+    }
+
+    private void UpdateDestinationBuffer(char[] destinationBuffer, int numberOfReadChars)
+    {
+        for (int i = 0; i < Math.Min(numberOfReadChars, numberOfRemainingOfCharsToRead); ++i)
+        {
+            destinationBuffer[numberOfActualCharsRead] = buffer[i];
+            ++numberOfActualCharsRead;
+        }
+        numberOfRemainingOfCharsToRead -= numberOfReadChars;
+    }
+}
