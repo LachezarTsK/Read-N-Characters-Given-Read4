5fe09151bc0d16aff718e5e5a777deb2ff76d025
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-11-16 13:12:51 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -23,7 +23,7 @@ public:
         int numberOfReadChars = read4(buffer);
         updateDestinationBuffer(destinationBuffer, numberOfReadChars);
 
-        while (numberOfReadChars == 4 && numberOfRemainingOfCharsToRead > 0) {
+        while (numberOfReadChars == MAX_READ_CHARS_PER_CALL_OF_READ4 && numberOfRemainingOfCharsToRead > 0) {
             numberOfReadChars = read4(buffer);
             updateDestinationBuffer(destinationBuffer, numberOfReadChars);
         }
